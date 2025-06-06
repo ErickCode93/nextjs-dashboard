155b395f945915e4310d1dbd9bf7593c1b812149
ErickCode93 <erickalvacontact@gmail.com>
2025-06-06 12:59:43 -0600

    customers module

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("");
+
+  return (
+    <>
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </>
+  );
 }
